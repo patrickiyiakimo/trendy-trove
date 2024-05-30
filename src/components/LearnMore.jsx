e5e513842416b74475cf29922a1dd36fc461129c
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-05-30 14:18:21 +0100

    Update LearnMore.jsx

--- src/components/LearnMore.jsx
+++ src/components/LearnMore.jsx
@@ -5,11 +5,11 @@ const LearnMore = () => {
   return (
     <div className="">
       <div className="pt-40 ">
-        <h1 className="text-center font-bold text-6xl pb-10">
+        <h1 className="text-center font-bold text-2xl md:text-6xl pb-10">
           Welcome to <span className="text-green-600">Furnex</span>
         </h1>
-        <p className="px-96 pb-10">
-          At Furnex, we believe that furniture is more than just a fuctional
+        <p className="ml-10 mr-10 md:px-96 pb-10">
+          At Furnex, we believe that furniture is more than just a functional
           necessity. It's an expression of style, a reflection of personality,
           and a cornerstone of comfort. Our mission is to provide high-quality
           furniture that enhances your living space and transforms your house
