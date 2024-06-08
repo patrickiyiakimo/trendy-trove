ce9376af957afaf20c6a5888fba39ed936d74bdd
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-06-08 14:12:29 +0100

    added a chat section to the customers testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -11,6 +11,7 @@ const Testimonials = () => {
       <h1 className="text-center font-semibold text-2xl md:text-6xl pb-10">
         Our Client Reviews
       </h1>
+
       <div className="avatar-group mb-10 flex items-center justify-center -space-x-6 rtl:space-x-reverse">
         <div className="avatar">
           <div className="w-12">
@@ -33,8 +34,42 @@ const Testimonials = () => {
           </div>
         </div>
       </div>
+      {/* the chat section */}
+      <div className="chat chat-start">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS chat bubble component"
+              src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+            />
+          </div>
+        </div>
+        <div className="chat-header">
+          Lona
+          <time className="text-xs opacity-50">12:45</time>
+        </div>
+        <div className="chat-bubble">
+          Furnex makes the best furniture piece ever! :)
+        </div>
+        <div className="chat-footer opacity-50">Delivered</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-image avatar">
+          <div className="w-10 rounded-full">
+            <img src={hello} alt="hello" />
+          </div>
+        </div>
+        <div className="chat-header">
+          Furnex
+          <time className="text-xs opacity-50">12:46</time>
+        </div>
+        <div className="chat-bubble">
+          Thanks for the Compliment. We hope you shop with us again.
+        </div>
+        <div className="chat-footer opacity-50">Seen at 12:46</div>
+      </div>
       {/* card testimonials */}
-     <Testimonies />
+      <Testimonies />
     </div>
   );
 };
