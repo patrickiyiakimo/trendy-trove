f6967f124420e6b1cbb4ef6812eb8d9ce1b7b0c3
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-07-01 11:57:22 +0100

    i removed the cart items statements and the lorem ipsum words

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -6,10 +6,6 @@ const CartPage = () => {
       <h1 className="text-center font-bold text-2xl md:text-6xl pb-10 pt-48">
         Cart Items
       </h1>
-      <p>Take a look at the incredible items you have added to your cart:)</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, nihil.
-      </p>
     </div>
   );
 };
