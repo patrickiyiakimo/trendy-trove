e199f06abe8e6465c84bd73c9cadf7f2ef0ac45e
Patrick Iyiakimo <iyiakimopatrick2002@gmail.com>
2024-07-02 17:19:43 +0100

    i added an extra text to the cart page

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -6,6 +6,7 @@ const CartPage = () => {
       <h1 className="text-center font-bold text-2xl md:text-6xl pb-10 pt-48">
         Cart Items
       </h1>
+      <p>Take a look at the random items you added to your cart</p>
     </div>
   );
 };
